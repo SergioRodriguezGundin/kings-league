d2640ed41bd6a09961e1c8cb2905493bd030b45b
Sergio-rg <40829363+SergioRodriguezGundin@users.noreply.github.com>
2023-01-14 19:04:47 +0100

    endpoint to return team logo

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/serve-static.module";
 import leaderboard from "../db/leaderboard.json";
 
 const app = new Hono();
@@ -6,11 +7,14 @@ const app = new Hono();
 app.get("/", (ctx) => {
   return ctx.json([
     { endpoint: "/leaderboard", description: "return the leaderboard" },
+    { endpoint: "/static/logos/{name}", description: "return the team logo" },
   ]);
 });
 
 app.get("/leaderboard", (ctx) => {
   return ctx.json(leaderboard);
 });
+
+app.get("/static/*", serveStatic({ root: "./" }));
 
 export default app;
